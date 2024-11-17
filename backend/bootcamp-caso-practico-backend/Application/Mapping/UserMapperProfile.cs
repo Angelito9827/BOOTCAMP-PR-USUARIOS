243081e42541a2a056e2ca442c829b7788a1b30e
Angel Hernandez <angelillohdez@gmail.com>
2024-11-18 00:36:46 +0100

    feat(back): create User entity with DTO, repository, services, controller, and database configuration

---
+++ backend/bootcamp-caso-practico-backend/Application/Mapping/UserMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using bootcamp_caso_practico_backend.Application.Dtos;
+using bootcamp_caso_practico_backend.Domain.Entities;
+
+namespace bootcamp_caso_practico_backend.Application.Mapping
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            CreateMap<User, UserDto>()
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name));
+            CreateMap<UserDto, User>();
+        }
+    }
+}
---
+++ backend/bootcamp-caso-practico-backend/Application/Services/UserServices.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using bootcamp_caso_practico_backend.Application.Dtos;
+using bootcamp_caso_practico_backend.Application.Services;
+using bootcamp_caso_practico_backend.Domain.Entities;
+using bootcamp_caso_practico_backend.Domain.Persistence;
+using bootcamp_framework.Application;
+using bootcamp_framework.Application.Services;
+
+namespace bootcamp_users_maintenance.Application.Services
+{
+    public class UserServices : GenericService<User, UserDto>, IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserServices(IUserRepository userRepository, IMapper mapper) : base(userRepository, mapper)
+        {
+            _userRepository = userRepository;
+        }
+
+        public PagedList<UserDto> GetUsersByCriteriaPaged(string? filter, PaginationParameters paginationParameters)
+        {
+            var users = _userRepository.GetUsersByCriteriaPaged(filter, paginationParameters);
+            return users;
+        }
+    }
+}
