87d68add3a6d1adea7fbfa6e4c2274a319f510c5
Angel Hernandez <angelillohdez@gmail.com>
2024-11-18 11:41:31 +0100

    feat(front): initial configuration

--- frontend/bootcamp-caso-practico-frontend/src/app/app.module.ts
+++ frontend/bootcamp-caso-practico-frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryListComponent } from './entities/category/category-list/category-list.component';
+import { CategoryFormComponent } from './entities/category/category-form/category-form.component';
+import { CategoryComponent } from './entities/category/service/category/category.component';
+import { ItemListComponent } from './entities/item/item-list/item-list.component';
+import { ItemFormComponent } from './entities/item/item-form/item-form.component';
+import { ItemComponent } from './entities/item/service/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoryListComponent,
+    CategoryFormComponent,
+    CategoryComponent,
+    ItemListComponent,
+    ItemFormComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
